26ed90ae697b23b533e56360020f9067ee9d20b4
Iegor <duuurrr17@gmail.com>
2022-08-23 19:57:24 +0300

    add comments for functions

--- Matrix/matrix_sort.py
+++ Matrix/matrix_sort.py
@@ -2,6 +2,7 @@ from random import randrange
 
 
 def ascending_bubble_sort(_matrix: list, col_sum: list):
+    # sort for ascending
     for row in range(len(col_sum)):
         for elem in range(0, len(col_sum) - row - 1):
             if col_sum[elem] > col_sum[elem + 1]:
@@ -13,9 +14,10 @@ def ascending_bubble_sort(_matrix: list, col_sum: list):
                 col_sum[elem] = col_sum[elem + 1]
                 col_sum[elem + 1] = temp
     else:
+        # bubble sort for columns with special params
         for i in range(len(_matrix)):
-            for j in range(len(col_sum)):
-                for k in range(0, len(col_sum) - j - 1):
+            for j in range(len(_matrix)):
+                for k in range(0, len(_matrix) - j - 1):
                     if _matrix[k][i] > _matrix[k + 1][i] if i % 2 else _matrix[k][i] < _matrix[k + 1][i]:
                         temp = _matrix[k][i]
                         _matrix[k][i] = _matrix[k + 1][i]
@@ -23,12 +25,14 @@ def ascending_bubble_sort(_matrix: list, col_sum: list):
 
 
 def print_matrix(_matrix: list, col_sum: list, status='Unsorted'):
+    # print unsort matrix
     print(f'\n{status.capitalize()} matrix:')
     for row in range(len(_matrix)):
         for elem in range(len(_matrix[row])):
             print(f"{_matrix[row][elem]:<7}", end=" ")
         print()
     for res in range(len(col_sum)):
+        # print sorted matrix
         print(f'{col_sum[res]:<7}', end=" ")
     print()
 
