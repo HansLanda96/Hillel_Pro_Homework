522c12d53ddf7cefd0edd58b52611d0ea26bca76
Iegor <duuurrr17@gmail.com>
2022-08-26 23:19:33 +0300

    change select query

--- SQL_script/homework5_sql.sql
+++ SQL_script/homework5_sql.sql
@@ -1,6 +1,6 @@
 --Написать SELECT запрос для подсчёта общей суммы треков и их количества
 --сгруппировав данные по TrackId.
 
-SELECT TrackId, SUM(UnitPrice) as TotalPrice, SUM(Quantity) as TotalQuant
+SELECT TrackId, SUM(UnitPrice) as TotalPrice, COUNT(TrackId) as TotalTrackId
 FROM invoice_items
 GROUP BY TrackId;
